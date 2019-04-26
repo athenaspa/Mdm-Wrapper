e4b8196d3c6b7597bedc878e0146a8e9c9dd6709
ATHENA <athena.alonte@gmail.com>
2019-04-26 16:12:09 +0200

    debug config host

--- load.environment.php
+++ load.environment.php
@@ -3,6 +3,7 @@
  * This file is included very early. See autoload.files in composer.json and
  * https://getcomposer.org/doc/04-schema.md#files
  */
+
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Dotenv\Exception\PathException;
 use Composer\Autoload\ClassLoader;
@@ -17,17 +18,18 @@ try {
 
     // unix, linux, mac
     if (DIRECTORY_SEPARATOR == '/') {
-        $vendorPath = preg_replace('/^(.*)\/composer\/ClassLoader\.php$/', '$1', $reflector->getFileName() );
+        $vendorPath = preg_replace('/^(.*)\/composer\/ClassLoader\.php$/', '$1', $reflector->getFileName());
     }
 
     // windows
     if (DIRECTORY_SEPARATOR == '\\') {
-        $vendorPath = preg_replace('/^(.*)\\composer\\ClassLoader\.php$/', '$1', $reflector->getFileName() );
+        $vendorPath = preg_replace('/^(.*)\\\\composer\\\\ClassLoader\.php$/', '$1', $reflector->getFileName());
     }
 
-    if($vendorPath && is_dir($vendorPath)) {
-        $dotenv->load($vendorPath . '/../.env');
+    if ($vendorPath && is_dir($vendorPath)) {
+        $dotenv->load($vendorPath . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '.env');
     }
+
 } catch (PathException $e) {
     // Do nothing.
 } catch (ReflectionException $e) {
